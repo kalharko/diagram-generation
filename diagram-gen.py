639ea6dc45e0d2f1388707e45d9614519b948f18
Oscar Dewasmes <46170581+kalharko@users.noreply.github.com>
2025-06-07 23:44:42 +0200

    Parsing python (#1)

---
+++ diagram-gen.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+import re
+from re import Match
+from typing import Iterator
+
+from diagram_generation import parse_class_descriptions_from_module
+from diagram_generation.python.class_description import ClassDescription
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path",
+        help="path to the folder to treat",
+        type=Path,
+    )
+    args = parser.parse_args()
+
+    # Check argument validity
+    if not args.path.exists:
+        raise ValueError(f"Path {args.path} is not a valid path.")
+    if not args.path.is_dir:
+        raise ValueError(f"Path {args.path} is not a directory.")
+    if args.path.glob("__init__.py").__len__() == 0:
+        raise ValueError(f"Path {args.path} does not contain a `__init__.py` file.")
+
+    # Parse module informations
+    class_descriptions: list[ClassDescription] = parse_class_descriptions_from_module(path=args.path)
+
+    # Render class descriptions
+    for class_description in class_descriptions:
+        print(class_description)
---
+++ diagram_generation/python/__init__.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+import re
+from typing import Any
+
+
+class DescriptionABC(ABC):
+    """Abstract base class for object descriptions.
+    """
+    source: str  # source string from which the description is extracted
+    name: str  # name of the described object
+    docstring: str  # docstring of the described object
+
+    @abstractmethod
+    def __init__(self, source: str) -> None:
+        """
+        Args:
+            source (str): source string containing only the described object.
+        """
+        raise NotImplementedError
+
+    @property
+    def responsability(self) -> str:
+        """Parses the responsability paragraph in the function's docstring.
+        Returns:
+            str: The responsability paragraph, might be an empty string.
+        """
+
+        rematch = re.search(
+            pattern=r"(?s)(Responsabilities:.*?)(?:\"\"\"|\n\n)",  # TODO: check regex pattern
+            string=self.docstring,
+            flags=re.MULTILINE,
+        )
+        return "" if rematch is None else rematch.group(1)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.name})"
+
+    def __str__(self) -> str:
+        out = f"{self.name}:\n"
+        for key, value in self.__dict__.items():
+            if isinstance(value, list) and len(value) > 0:
+                out += f"{key}:\n"
+                val: Any
+                for val in value:
+                    out += f"- {val}\n"
+            else:
+                out += f"{key}: {value}\n"
+
+        return out
