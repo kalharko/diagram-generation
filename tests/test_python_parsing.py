7bb59226e7e0f203f844715d1605eab330681649
kalharko <46170581+kalharko@users.noreply.github.com>
2025-06-08 08:25:00 +0200

    🧪 add failing tests

---
+++ tests/test_python_parsing.py
@@ -0,0 +1,45 @@
+
+
+from diagram_generation.python.class_description import ClassDescription
+
+
+def test_python_class_parsing():
+    source = """
+asdfa
+asdf
+aa
+
+class A(B, C):
+    \"\"\"Dummy docstring
+    Responsabilities:
+    - Dummy responsability
+
+    end of docstring.
+    \"\"\"
+
+    member: member_typehint  # member_doc
+
+    @decorator
+    @second(decorator)
+    def __init__(self, a: list[a_typehint], b: b_typehing) -> return_val:
+        pass
+"""
+
+    class_description = ClassDescription(source)
+    assert class_description.name == "A"
+    assert class_description.docstring == """Dummy docstring
+    Responsabilities:
+    - Dummy responsability
+
+    end of docstring.
+    """
+    assert class_description.base_classes == ["B", "C"]
+    assert class_description.method_names == ["__init__"]
+    assert class_description.members == {"member": ("member_typehint", "member_doc")}
+
+    method0 = class_description.methods[0]
+    assert method0.name == "__init__"
+    assert method0.docstring == ""
+    assert method0.return_values == ["return_val"]
+    assert method0.parameters == {"self": None, "a": "list[a_typehint]", "b": "b_typehing"}
+    assert method0.decorators == ["decorator", "second(decorator)"]
