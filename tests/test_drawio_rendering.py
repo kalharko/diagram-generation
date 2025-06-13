e06c370cdc182bbe2e68981dd707ad40d9e41368
kalharko <46170581+kalharko@users.noreply.github.com>
2025-06-13 06:43:56 +0200

    :art: ruff

--- tests/test_drawio_rendering.py
+++ tests/test_drawio_rendering.py
@@ -1,6 +1,9 @@
-
-
-from diagram_generation.drawio.generate import class_full_drawio_table, get_members_value_height, get_methods_value_height, get_responsability_value_height
+from diagram_generation.drawio.generate import (
+    class_full_drawio_table,
+    get_members_value_height,
+    get_methods_value_height,
+    get_responsability_value_height,
+)
 from diagram_generation.python.class_description import ClassDescription
 from diagram_generation.python.function_description import FunctionDescription
 
@@ -32,7 +35,9 @@ def test_drawio_rendering() -> None:
     assert responsability_text == "Responsabilities:&lt;div&gt;- A responsability"
     assert members_text == "member: member_typehint"
     assert methods_text == "func(a, b) -&amp;gt; return_typehint"
-    assert xml == f"""<mxCell id="0" value="{class_description.name}" style="swimlane;fontStyle=0;childLayout=stackLayout;horizontal=1;startSize=30;horizontalStack=0;resizeParent=1;resizeParentMax=0;resizeLast=0;collapsible=1;marginBottom=0;whiteSpace=wrap;html=1;" vertex="1" parent="1">
+    assert (
+        xml
+        == f"""<mxCell id="0" value="{class_description.name}" style="swimlane;fontStyle=0;childLayout=stackLayout;horizontal=1;startSize=30;horizontalStack=0;resizeParent=1;resizeParentMax=0;resizeLast=0;collapsible=1;marginBottom=0;whiteSpace=wrap;html=1;" vertex="1" parent="1">
         <mxGeometry x="60" y="180" width="220" height="110" as="geometry"/>
     </mxCell>
     <mxCell id="1" value="{responsability_text}" style="text;strokeColor=none;fillColor=none;align=left;verticalAlign=middle;spacingLeft=4;spacingRight=4;overflow=hidden;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rotatable=0;whiteSpace=wrap;html=1;" vertex="1" parent="0">
@@ -45,3 +50,4 @@ def test_drawio_rendering() -> None:
         <mxGeometry y="80" width="220" height="{methods_height}" as="geometry"/>
     </mxCell>
 """
+    )
